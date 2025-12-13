605f4f9e5c07a423d9a0970f0ac9ab52247e2384
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2025-12-13 15:20:31 +0530

    Add  main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
       version = "~> 4.56.0"
     }
   }
-  required_version = ">= 1.1.0"
+  required_version = ">= 1.0.0"
 }
 
 provider "azurerm" {
@@ -17,17 +17,23 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-resource "azurerm_storage_account" "sa" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = [var.vnet_address_space]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.subnet_address]
 }
 
 resource "azurerm_app_service_plan" "asp" {
-  name                = var.app_service_plan_name
-  location            = var.location
+  name                = var.asp_name
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku {
     tier = "Basic"
@@ -35,31 +41,79 @@ resource "azurerm_app_service_plan" "asp" {
   }
 }
 
-resource "azurerm_app_service" "app" {
+resource "azurerm_app_service" "app_service" {
   name                = var.app_service_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.asp.id
 }
 
-resource "azurerm_application_insights" "ai" {
-  name                = var.app_insights_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  application_type    = "web"
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
-resource "azurerm_key_vault" "kv" {
-  name                = var.key_vault_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  tenant_id           = var.tenant_id
-  sku_name            = "standard"
+resource "azurerm_storage_container" "blob_container" {
+  name                  = var.blob_container_name
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
 }
 
-resource "azurerm_eventhub_namespace" "ehn" {
-  name                = var.eventhub_namespace_name
+resource "azurerm_key_vault" "key_vault" {
+  name                        = var.key_vault_name
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  tenant_id                   = var.tenant_id
+  sku_name                    = "standard"
+}
+
+resource "azurerm_function_app" "function_app" {
+  name                       = var.function_app_name
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  app_service_plan_id        = azurerm_app_service_plan.asp.id
+  storage_account_name       = azurerm_storage_account.storage.name
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+  os_type                    = "linux"
+  https_only                 = true
+  site_config {
+    linux_fx_version = "DOCKER|mcr.microsoft.com/azure-functions/dotnet:3.0"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                            = var.vm_name
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  size                            = "Standard_B1s"
+  admin_username                  = var.vm_admin_username
+  admin_password                  = var.vm_admin_password
+  network_interface_ids           = [azurerm_network_interface.nic.id]
+  disable_password_authentication = false
+  os_disk {
+    name                 = "osdisk-example"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    disk_size_gb         = 30
+  }
+  source_image_reference {
+    publisher = "canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = var.nic_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "Standard"
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
