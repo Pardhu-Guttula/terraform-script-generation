c213db94bd86f276360d2a8918b91b59025b491d
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-08 20:25:13 +0530

    Add  main.tf

--- main.tf
+++ main.tf
@@ -5,85 +5,45 @@ terraform {
       version = "~> 4.56.0"
     }
   }
+  required_version = ">= 1.0.0"
 }
 
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "banking-portal-rg"
-  location = "East US"
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
 }
 
-# Authentication and Security
-resource "azurerm_key_vault" "kv" {
-  name                = "banking-keyvault"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku_name            = "standard"
-}
-
-resource "azurerm_app_service_environment_v3" "ase" {
-  name                = "banking-ase"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-# Data Layer
-resource "azurerm_sql_server" "sql" {
-  name                         = "bankingsqlserver"
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
-  version                      = "12.0"
-  administrator_login          = "sqladmin"
-  administrator_login_password = random_password.sql_password.result
-}
-
-resource "azurerm_sql_database" "sql_db" {
-  name                = "banking-sqldb"
-  resource_group_name = azurerm_sql_server.sql.resource_group_name
-  location            = azurerm_sql_server.sql.location
-  server_name         = azurerm_sql_server.sql.name
-  requested_service_objective_name = "S0"
-}
-
-resource "azurerm_cosmosdb_account" "cosmos" {
-  name                = "banking-cosmosdb"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  offer_type          = "Standard"
-  kind                = "GlobalDocumentDB"
-}
-
-resource "azurerm_storage_account" "storage" {
-  name                     = "bankingstorage"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageacc"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-# Monitoring and Logging
-resource "azurerm_monitor_diagnostic_setting" "diag" {
-  name                       = "banking-diag"
-  target_resource_id         = azurerm_app_service_environment_v3.ase.id
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+resource "azurerm_monitor_diagnostic_setting" "example" {
+  name                       = "example_diag"
+  target_resource_id         = azurerm_storage_account.example.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
+
   enabled_log {
-    category = "AppServiceHTTPLogs"
+    category = "AuditEvent"
+    enabled  = true
   }
+
   metric {
     category = "AllMetrics"
+    enabled  = true
   }
 }
 
-resource "azurerm_log_analytics_workspace" "law" {
-  name                = "banking-log-analytics"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_log_analytics_workspace" "example" {
+  name                = "example-workspace"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   sku                 = "PerGB2018"
-}
-
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
 }
