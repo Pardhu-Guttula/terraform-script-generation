680c647b837edd2485e9b6c685530316ac7e6ed7
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-08 23:10:28 +0530

    Add  main.tf

--- main.tf
+++ main.tf
@@ -11,43 +11,64 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_function_app" "example" {
-  name                       = "example-functionapp"
-  location                   = azurerm_resource_group.example.location
-  resource_group_name        = azurerm_resource_group.example.name
-  application_insights_id    = azurerm_application_insights.example.id
-  app_service_plan_id        = azurerm_app_service_plan.example.id
-  storage_account_name       = azurerm_storage_account.example.name
-  storage_account_access_key = azurerm_storage_account.example.primary_access_key
-}
-
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_app_service_plan" "example" {
-  name                = "example-plan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  sku {
-    tier = "Dynamic"
-    size = "Y1"
-  }
-}
-
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacc"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+resource "azurerm_storage_account" "storage" {
+  name                     = "${var.storage_account_name}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_application_insights" "example" {
-  name                = "example-ai"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  application_type    = "web"
+resource "azurerm_sql_server" "sqlserver" {
+  name                         = var.sql_server_name
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = var.sql_admin_user
+  administrator_login_password = var.sql_admin_password
 }
 
+resource "azurerm_sql_database" "sqldb" {
+  name                = var.sql_database_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  server_name         = azurerm_sql_server.sqlserver.name
+  requested_service_objective_name = "S0"
+}
+
+resource "azurerm_function_app" "function" {
+  name                       = var.function_app_name
+  resource_group_name        = azurerm_resource_group.rg.name
+  location                   = azurerm_resource_group.rg.location
+  storage_account_name       = azurerm_storage_account.storage.name
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+  os_type                    = "linux"
+  runtime_stack              = "node"
+}
+
+resource "azurerm_logic_app_workflow" "logic_app" {
+  name                = var.logic_app_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+resource "azurerm_api_management" "apim" {
+  name                = var.apim_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  publisher_name      = var.publisher_name
+  publisher_email     = var.publisher_email
+  sku_name            = "Developer_1"
+}
+
+resource "azurerm_application_insights" "app_insights" {
+  name                = var.app_insights_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  application_type    = "web"
+}
