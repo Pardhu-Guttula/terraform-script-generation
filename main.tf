86c558b19e1abacbab954c6958cb9d0e1ef4dec3
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-08 19:42:06 +0530

    Add  main.tf

--- main.tf
+++ main.tf
@@ -11,44 +11,36 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "East US"
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestoracc"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+resource "azurerm_storage_account" "sa" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_key_vault" "example" {
-  name                     = "examplekeyvault"
-  location                 = azurerm_resource_group.example.location
-  resource_group_name      = azurerm_resource_group.example.name
-  tenant_id                = data.azurerm_client_config.current.tenant_id
-  sku_name                 = "standard"
+resource "azurerm_application_insights" "appinsights" {
+  name                = var.app_insights_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
 }
 
-resource "azurerm_monitor_diagnostic_setting" "example" {
-  name                       = "example-diagnostics"
-  target_resource_id         = azurerm_key_vault.example.id
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
-
-  enabled_log {
-    category = "AuditEvent"
-  }
-
-  metric {
-    category = "AllMetrics"
-  }
+resource "azurerm_key_vault" "kv" {
+  name                = var.key_vault_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name            = "standard"
+  tenant_id           = var.tenant_id
 }
 
-resource "azurerm_log_analytics_workspace" "example" {
-  name                = "example-workspace"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  sku                 = "PerGB2018"
+resource "azurerm_eventgrid_topic" "topic" {
+  name                = var.eventgrid_topic_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
