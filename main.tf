0a2e09d44fb5dd2af29817c03b8cffd6151361e5
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-08 18:41:41 +0530

    Add  main.tf

--- main.tf
+++ main.tf
@@ -1,188 +1,86 @@
-terraform {
+provider 'azurerm' {
+  features {}
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.56.0"
+      source  = 'hashicorp/azurerm'
+      version = '~> 4.56.0'
     }
   }
-  required_version = ">= 1.1.0"
 }
-
-provider "azurerm" {
-  features {}
+resource 'azurerm_resource_group' 'rg' {
+  name     = 'rg-sample'
+  location = 'East US'
 }
-
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-banking-portal"
-  location = "East US"
-}
-
-resource "azurerm_app_service_plan" "asp" {
-  name                = "appserviceplan"
+resource 'azurerm_app_service_plan' 'asp' {
+  name                = 'asp-sample'
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku {
-    tier = "Standard"
-    size = "S1"
+    tier = 'Basic'
+    size = 'B1'
   }
 }
-
-resource "azurerm_app_service" "app" {
-  name                = "web-app"
+resource 'azurerm_app_service' 'app_service' {
+  name                = 'app-sample'
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.asp.id
-}
-
-resource "azurerm_function_app" "functions" {
-  name                       = "func-app"
-  location                   = azurerm_resource_group.rg.location
-  resource_group_name        = azurerm_resource_group.rg.name
-  app_service_plan_id        = azurerm_app_service_plan.asp.id
-  storage_account_name       = azurerm_storage_account.sa.name
-  storage_account_access_key = azurerm_storage_account.sa.primary_access_key
-  version                    = "~3"
-}
-
-resource "azurerm_storage_account" "sa" {
-  name                     = "storageaccount"
-  location                 = azurerm_resource_group.rg.location
-  resource_group_name      = azurerm_resource_group.rg.name
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_sql_server" "sql" {
-  name                         = "sqlserver"
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
-  version                      = "12.0"
-  administrator_login          = "sqladmin"
-  administrator_login_password = "P@ssw0rd1234"
-}
-
-resource "azurerm_sql_database" "sqldb" {
-  name                = "sqldatabase"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  server_name         = azurerm_sql_server.sql.name
-  sku_name            = "S1"
-}
-
-resource "azurerm_cosmosdb_account" "cosmos" {
-  name                = "cosmosdb"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  offer_type          = "Standard"
-  consistency_policy {
-    consistency_level = "Session"
+  site_config {
+    always_on = true
   }
 }
-
-resource "azurerm_eventgrid_domain" "eventgrid" {
-  name                = "eventgriddomain"
+resource 'azurerm_key_vault' 'kv' {
+  name                = 'kv-sample'
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = 'standard'
+  soft_delete_enabled = true
 }
-
-resource "azurerm_logic_app_workflow" "logicapp" {
-  name                = "logicapp"
+resource 'azurerm_api_management' 'apim' {
+  name                = 'apim-sample'
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  publisher_name      = 'pub-name'
+  publisher_email     = 'pub-email@example.com'
+  sku_name            = 'Developer_1'
 }
-
-resource "azurerm_api_management" "apim" {
-  name                = "apimanagement"
+resource 'azurerm_sql_server' 'sql_server' {
+  name                         = 'sql-server-sample'
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = '12.0'
+  administrator_login          = 'adminuser'
+  administrator_login_password = 'AdminPassword123!'
+}
+resource 'azurerm_sql_database' 'sql_db' {
+  name                = 'sqldb-sample'
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  server_name         = azurerm_sql_server.sql_server.name
+  sku_name            = 'S0'
+}
+resource 'azurerm_cosmosdb_account' 'cosmosdb' {
+  name                = 'cosmosdb-sample'
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  sku_name            = "Developer_1"
-  publisher_name      = "My Company"
-  publisher_email     = "admin@mycompany.com"
+  offer_type          = 'Standard'
+  kind                = 'GlobalDocumentDB'
+  consistency_policy {
+    consistency_level = 'Eventual'
+  }
 }
-
-resource "azurerm_key_vault" "keyvault" {
-  name                = "keyvault"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  tenant_id           = "00000000-0000-0000-0000-000000000000"
-  sku_name            = "standard"
-}
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "myVnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet1"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_network_security_group" "nsg" {
-  name                = "nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_network_interface" "nic" {
-  name                = "nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  subnet_id           = azurerm_subnet.subnet.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
-resource "azurerm_application_gateway" "appgw" {
-  name                = "appgateway"
+resource 'azurerm_notification_hub_namespace' 'nh_namespace' {
+  name                = 'nh-namespace-sample'
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku {
-    name     = "Standard_Small"
-    tier     = "Standard"
-    capacity = 2
-  }
-  gateway_ip_configuration {
-    name      = "configuration"
-    subnet_id = azurerm_subnet.subnet.id
-  }
-  frontend_port {
-    name = "frontendPort"
-    port = 80
-  }
-  frontend_ip_configuration {
-    name                 = "frontendConfig"
-    public_ip_address_id = azurerm_public_ip.appgw_pip.id
-  }
-  backend_address_pool {
-    name = "backendPool"
-  }
-  http_listener {
-    name                           = "httpListener"
-    frontend_ip_configuration_name = "frontendConfig"
-    frontend_port_name             = "frontendPort"
-  }
-  request_routing_rule {
-    name                       = "routingRule"
-    rule_type                  = "Basic"
-    http_listener_name         = "httpListener"
-    backend_address_pool_name  = "backendPool"
-    backend_http_settings_name = "backendHttpSettings"
-  }
-  backend_http_settings {
-    name                  = "backendHttpSettings"
-    port                  = "80"
-    protocol              = "Http"
-    cookie_based_affinity = "Disabled"
+    name     = 'Basic'
+    capacity = 1
   }
 }
-
-resource "azurerm_public_ip" "appgw_pip" {
-  name                = "appgwpip"
-  location            = azurerm_resource_group.rg.location
+resource 'azurerm_notification_hub' 'nhub' {
+  name                = 'nhub-sample'
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
-}
+  namespace_name      = azurerm_notification_hub_namespace.nh_namespace.name
+}}
