dbf01b643d886df7d6f768d4868726ee949b4c2e
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-09 10:38:50 +0530

    Add  main.tf

--- main.tf
+++ main.tf
@@ -5,70 +5,118 @@ terraform {
       version = "~> 4.56.0"
     }
   }
+  required_version = ">= 0.15.0"
 }
 
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
 }
 
-resource "azurerm_storage_account" "storage" {
-  name                     = "${var.storage_account_name}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
 }
 
-resource "azurerm_sql_server" "sqlserver" {
-  name                         = var.sql_server_name
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
+resource "azurerm_app_service" "example" {
+  name                = "example-app"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+  site_config {
+    app_settings = {
+      "WEBSITE_RUN_FROM_PACKAGE" = "1"
+    }
+  }
+}
+
+resource "azurerm_sql_server" "example" {
+  name                         = "example-sqlserver"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
   version                      = "12.0"
   administrator_login          = var.sql_admin_user
   administrator_login_password = var.sql_admin_password
 }
 
-resource "azurerm_sql_database" "sqldb" {
-  name                = var.sql_database_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  server_name         = azurerm_sql_server.sqlserver.name
-  requested_service_objective_name = "S0"
+resource "azurerm_sql_database" "example" {
+  name                = "example-db"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  server_name         = azurerm_sql_server.example.name
+  sku_name            = "S0"
 }
 
-resource "azurerm_function_app" "function" {
-  name                       = var.function_app_name
-  resource_group_name        = azurerm_resource_group.rg.name
-  location                   = azurerm_resource_group.rg.location
-  storage_account_name       = azurerm_storage_account.storage.name
-  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
-  os_type                    = "linux"
-  runtime_stack              = "node"
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageacct"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
-resource "azurerm_logic_app_workflow" "logic_app" {
-  name                = var.logic_app_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+resource "azurerm_monitor" "example" {
+  name                = "example-monitor"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
 
-resource "azurerm_api_management" "apim" {
-  name                = var.apim_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  publisher_name      = var.publisher_name
-  publisher_email     = var.publisher_email
+resource "azurerm_monitor_diagnostic_setting" "example" {
+  name                        = "example-monitor-diagnostic"
+  target_resource_id          = azurerm_app_service.example.id
+  log_analytics_workspace_id  = var.log_analytics_workspace_id
+  enabled_log {
+    category = "AppServiceHTTPLogs"
+    enabled  = true
+  }
+  metric {
+    category = "AllMetrics"
+    enabled  = true
+  }
+}
+
+resource "azurerm_log_analytics_workspace" "example" {
+  name                = "example-law"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku                 = "PerGB2018"
+}
+
+resource "azurerm_key_vault" "example" {
+  name                        = "example-kv"
+  location                    = azurerm_resource_group.example.location
+  resource_group_name         = azurerm_resource_group.example.name
+  tenant_id                   = var.tenant_id
+  sku_name                    = "standard"
+}
+
+resource "azurerm_api_management" "example" {
+  name                = "example-apim"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  publisher_name      = "Example"
+  publisher_email     = "example@example.com"
   sku_name            = "Developer_1"
 }
 
-resource "azurerm_application_insights" "app_insights" {
-  name                = var.app_insights_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  application_type    = "web"
+resource "azurerm_notification_hub_namespace" "example" {
+  name                = "example-nhns"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku_name            = "Free"
+}
+
+resource "azurerm_notification_hub" "example" {
+  name                = "example-nh"
+  resource_group_name = azurerm_resource_group.example.name
+  namespace_name      = azurerm_notification_hub_namespace.example.name
 }
