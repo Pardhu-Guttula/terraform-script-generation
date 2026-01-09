c86d748e0fef0fda3f418012167de50f9cae67d0
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-09 14:08:35 +0530

    Add  variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,23 @@
-variable "tenant_id" {
-  description = "The Tenant ID for the Azure subscription."
+variable "subscription_id" {
+  description = "The Subscription ID to deploy to (optional)."
   type        = string
+  default     = "[SUBSCRIPTION_ID]"
 }
 
-variable "sql_admin_user" {
-  description = "The admin username for the SQL server."
+variable "tenant_id" {
+  description = "The Tenant ID to use for authorization."
   type        = string
+  default     = "[TENANT_ID]"
+}
+
+variable "sql_admin_username" {
+  description = "The administrator username for SQL Server."
+  type        = string
+  default     = "[SQL_ADMIN]"
 }
 
 variable "sql_admin_password" {
-  description = "The admin password for the SQL server."
+  description = "The administrator password for SQL Server."
   type        = string
-}
-
-variable "log_analytics_workspace_id" {
-  description = "The ID of the Log Analytics workspace for monitoring."
-  type        = string
+  default     = "[SQL_PASSWORD]"
 }
