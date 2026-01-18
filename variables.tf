c15fa9d1214c05abc6f373175a5641d4fca2829d
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-18 16:52:18 +0530

    Add  variables.tf

--- variables.tf
+++ variables.tf
@@ -1,23 +1,4 @@
-variable "subscription_id" {
-  description = "The Subscription ID to deploy to (optional)."
-  type        = string
-  default     = "[SUBSCRIPTION_ID]"
-}
-
-variable "tenant_id" {
-  description = "The Tenant ID to use for authorization."
-  type        = string
-  default     = "[TENANT_ID]"
-}
-
-variable "sql_admin_username" {
-  description = "The administrator username for SQL Server."
-  type        = string
-  default     = "[SQL_ADMIN]"
-}
-
 variable "sql_admin_password" {
-  description = "The administrator password for SQL Server."
+  description = "The password for the SQL Server admin user."
   type        = string
-  default     = "[SQL_PASSWORD]"
 }
