84d8e5523506e987b4447a816c66b326504fefbf
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-19 16:33:03 +0530

    Add  variables.tf

--- variables.tf
+++ variables.tf
@@ -1,46 +1,9 @@
-variable "resource_group_name" {
-  description = "The name of the resource group."
+variable "tenant_id" {
+  description = "The ID of the Tenant to which the subscription belongs."
   type        = string
 }
 
-variable "location" {
-  description = "The location of the resource group."
-  type        = string
-  default     = "East US"
-}
-
-variable "app_service_plan_name" {
-  description = "The name of the App Service Plan."
-  type        = string
-}
-
-variable "app_name" {
-  description = "The name of the App Service."
-  type        = string
-}
-
-variable "storage_account_name" {
-  description = "The name of the Storage Account."
-  type        = string
-}
-
-variable "sql_server_name" {
-  description = "The name of the SQL Server."
-  type        = string
-}
-
-variable "sql_admin_username" {
-  description = "The admin username for the SQL Server."
-  type        = string
-}
-
-variable "sql_admin_password" {
-  description = "The admin password for the SQL Server."
-  type        = string
-  sensitive   = true
-}
-
-variable "sql_database_name" {
-  description = "The name of the SQL Database."
+variable "sql_password" {
+  description = "The password for the SQL administrator user."
   type        = string
 }
