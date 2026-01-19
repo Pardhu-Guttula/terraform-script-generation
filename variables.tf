6b3be703970a010f718f187ab54584d40ca4e9bb
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-19 22:55:44 +0530

    Add  variables.tf

--- variables.tf
+++ variables.tf
@@ -1,40 +1,9 @@
-variable "resource_group_name" {
-  description = "Name of the resource group"
+variable 'sql_admin_username' {
+  description = 'The username for the SQL server administrator.'
   type        = string
 }
 
-variable "location" {
-  description = "Location of the resources"
-  type        = string
-}
-
-variable "sql_admin_username" {
-  description = "Administrator username for SQL"
-  type        = string
-}
-
-variable "sql_admin_password" {
-  description = "Administrator password for SQL"
-  type        = string
-  sensitive   = true
-}
-
-variable "tenant_id" {
-  description = "Tenant ID for Azure"
-  type        = string
-}
-
-variable "role_definition_name" {
-  description = "Name of the role definition"
-  type        = string
-}
-
-variable "organization_name" {
-  description = "Name of the organization for Azure DevOps"
-  type        = string
-}
-
-variable "principal_id" {
-  description = "Principal ID for role assignment"
+variable 'sql_admin_password' {
+  description = 'The password for the SQL server administrator.'
   type        = string
 }
