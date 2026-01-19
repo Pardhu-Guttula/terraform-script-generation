f1fda1b5014f8202797cc8b14475922d3df39c36
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-19 13:57:58 +0530

    Add  variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,46 @@
 variable "resource_group_name" {
-  description = "The name of the resource group in which to create the resources."
+  description = "The name of the resource group."
   type        = string
 }
 
 variable "location" {
-  description = "The location/region where the resources will be created."
+  description = "The location of the resource group."
   type        = string
   default     = "East US"
 }
 
-variable "tenant_id" {
-  description = "The Tenant ID for the Azure Key Vault."
+variable "app_service_plan_name" {
+  description = "The name of the App Service Plan."
+  type        = string
+}
+
+variable "app_name" {
+  description = "The name of the App Service."
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "The name of the Storage Account."
+  type        = string
+}
+
+variable "sql_server_name" {
+  description = "The name of the SQL Server."
+  type        = string
+}
+
+variable "sql_admin_username" {
+  description = "The admin username for the SQL Server."
   type        = string
 }
 
 variable "sql_admin_password" {
-  description = "The password for the SQL admin user."
+  description = "The admin password for the SQL Server."
+  type        = string
+  sensitive   = true
+}
+
+variable "sql_database_name" {
+  description = "The name of the SQL Database."
   type        = string
 }
