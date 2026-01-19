4330cbd4954c6668eb71fddcee5ff7c8361bbbaf
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-19 11:53:17 +0530

    Add  variables.tf

--- variables.tf
+++ variables.tf
@@ -1,16 +1,20 @@
+variable "resource_group_name" {
+  description = "The name of the resource group in which to create the resources."
+  type        = string
+}
+
 variable "location" {
-  description = "Location of the resources."
+  description = "The location/region where the resources will be created."
   type        = string
   default     = "East US"
 }
 
-variable "log_analytics_workspace_id" {
-  description = "Log Analytics Workspace ID for monitoring."
+variable "tenant_id" {
+  description = "The Tenant ID for the Azure Key Vault."
   type        = string
 }
 
-variable "sql_password" {
-  description = "Admin password for SQL Server."
+variable "sql_admin_password" {
+  description = "The password for the SQL admin user."
   type        = string
-  sensitive   = true
 }
