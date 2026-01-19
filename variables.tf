50fef6f6dc23008aba9a2f3f9e45fbca0c314c95
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-19 22:13:47 +0530

    Add  variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,15 @@
-variable "tenant_id" {
-  description = "The ID of the Tenant to which the subscription belongs."
+variable 'resource_group_name' {
+  description = 'The name of the resource group in which to create resources'
   type        = string
 }
 
-variable "sql_password" {
-  description = "The password for the SQL administrator user."
+variable 'location' {
+  description = 'The Azure region where the resources will be created'
   type        = string
+}
+
+variable 'sql_password' {
+  description = 'The password for the SQL Server administrator'
+  type        = string
+  sensitive   = true
 }
