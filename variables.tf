592c9f3eab384b8356e609a7247c656fc4939896
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-19 22:26:43 +0530

    Add  variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,9 @@
-variable 'resource_group_name' {
-  description = 'The name of the resource group in which to create resources'
+variable "sql_admin_user" {
+  description = "The admin username for SQL Server"
   type        = string
 }
 
-variable 'location' {
-  description = 'The Azure region where the resources will be created'
+variable "sql_admin_password" {
+  description = "The admin password for SQL Server"
   type        = string
-}
-
-variable 'sql_password' {
-  description = 'The password for the SQL Server administrator'
-  type        = string
-  sensitive   = true
 }
